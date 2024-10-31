ee52554cd851671a0c5672b3b244ea9f2fb9938d
cwalke6 <walkercd21@gmail.com>
2024-10-31 13:53:51 -0400

    Creating a branch where I can test individual features on a blank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import './Login.css'
-import Login from './Login.jsx'
-import Messages from './Messages';
-import Gallery from './Gallery';
+import './Login.css';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <div className='main-layout'>
-
-      <div className={`main-messages ${isOpen ? 'open' : ''}`}>
-        <div className='message-header'> 
-          Messages 
-        </div>
-        <Messages />
-      </div>
-
-      <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="burger"></div>
-        <div className="burger"></div>
-        <div className="burger"></div>
-      </div>
-
-      <div className='line'></div>
-      
-      <Gallery />
-      
-      <div>
-        <Login />
-      </div>
+      /* Empty page */
     </div>
-  );
-};
-
-export default App;
+  )
+}

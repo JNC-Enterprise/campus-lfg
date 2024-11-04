49b13a7e6f03ac477e00852747cd6ffdeaa832d4
cwalke6 <walkercd21@gmail.com>
2024-11-04 17:39:31 -0500

    Updated the ui look of the group creation screen. Should be much better now

--- src/Group/GroupCreation.jsx
+++ src/Group/GroupCreation.jsx
@@ -5,29 +5,32 @@ function GroupCreation() {
 
   /* Should eventually make these drop down menus */
   return(
-    <div className='group-creation-container'>
-      <button /*onClick={}*/ className='group-creator-text'>
-      CREATE GROUP
-      </button>
-      <button /*onClick={}*/ className='group-game-selector'>
-      SELECT GAME
-      </button>
-      <button /*onClick={}*/ className='group-rank-selector'>
-      SELECT RANK
-      </button>
-      <button /*onClick={}*/ className='group-mode-selector'>
-      SELECT MODE
-      </button>
-      <button /*onClick={}*/ className='group-players-selector'>
-      # OF PLAYERS
-      </button>
-      <button /*onClick={}*/ className='group-title-selector'>
-      &lt;ENTER GROUP TITLE&gt;
-      </button>
-      <button /*onClick={}*/ className='group-done-button'>
-      DONE
-      </button>
-
+    <div className='group-creation-page'>
+      {/* Should probably remove this group-creation-page div. For now it is there to test the smaller, 
+      group-creation-container, still being in the center */}
+      <div className='group-creation-container'>
+        <button /*onClick={}*/ className='group-creator-text'>
+        CREATE GROUP
+        </button>
+        <button /*onClick={}*/ className='group-game-selector'>
+        SELECT GAME
+        </button>
+        <button /*onClick={}*/ className='group-rank-selector'>
+        SELECT RANK
+        </button>
+        <button /*onClick={}*/ className='group-mode-selector'>
+        SELECT MODE
+        </button>
+        <button /*onClick={}*/ className='group-players-selector'>
+        # OF PLAYERS
+        </button>
+        <button /*onClick={}*/ className='group-title-selector'>
+        &lt;ENTER GROUP TITLE&gt;
+        </button>
+        <button /*onClick={}*/ className='group-done-button'>
+        DONE
+        </button>
+      </div>
     </div>
   );
 };

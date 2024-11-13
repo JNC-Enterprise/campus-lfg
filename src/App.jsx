510f66e2e51f74471505c72494079dfcb5e8c239
cwalke6 <walkercd21@gmail.com>
2024-11-13 17:11:40 -0500

    group creation screen is now a pop up. Need to have a button that will open it up but also have the X button to close it.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import './Login.css';
 import GroupCreation from './Group/GroupCreation.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);

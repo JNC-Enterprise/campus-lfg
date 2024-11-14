bace4e08410300908694ad315833afdc7e1f0f37
cwalker6 <walkercd21@gmail.com>
2024-11-13 19:57:28 -0500

    Made it that the game is auto selected in group creation and adding game specific ranks to be selected. Need to add more ranks to each game and make them more specific.

--- src/Group/GroupCreation.jsx
+++ src/Group/GroupCreation.jsx
@@ -1,46 +1,44 @@
 import './GroupCreation.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 
-function GroupCreation({ toggleVisibility, defaultGame}) {
+function GroupCreation({ toggleVisibility, defaultGame }) {
   const [groupTitle, setGroupTitle] = useState('');
   const [selectedGame, setSelectedGame] = useState(defaultGame || 'SELECT GAME');
   const [numPlayers, setNumPlayers] = useState('?');
   const [groupGamemode, setGamemode] = useState('SELECT MODE');
   const [userRank, setUserRank] = useState('SELECT RANK');
 
-  const groupTitleChange = (e) => {
-    setGroupTitle(e.target.value);
+  // Define rank options for each game
+  const rankOptions = {
+    "Valorant": ["Iron 1", "Iron 2", "Iron 3", "Bronze 1", "Bronze 2", "Bronze 3", "Silver", "Gold", "Platinum", "Diamond", "Immortal", "Radiant"],
+    "Overwatch": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster", "Top 500"],
+    "Rainbow Six Siege": ["Copper", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Champion"],
+    // Add more games with specific ranks as needed
   };
 
   const handleSelect = (game) => {
     setSelectedGame(game);
+    setUserRank('SELECT RANK'); // Reset rank when the game changes
+  };
+
+  const handleRank = (rank) => {
+    setUserRank(rank);
   };
 
   const handlePlayers = (players) => {
     setNumPlayers(players);
   };
 
-  const handleGamemode = (groupGamemode) => {
-    setGamemode(groupGamemode);
+  const handleGamemode = (gamemode) => {
+    setGamemode(gamemode);
   };
 
-  const handleRank = (userRank) => {
-    setUserRank(userRank);
+  const groupTitleChange = (e) => {
+    setGroupTitle(e.target.value);
   };
 
-  const gameSelectedCheck = (e) => {
-    if(selectedGame === 'SELECT GAME') {
-      e.preventDefault();
-      alert('Please select a game first!');
-    }
-  };
-
-  const handleGroupCreation = (e) => {
-    // Group creation logic here
-  };
-
-  return(
+  return (
     <div>
       <div className='group-creation-container'>
         <button className='group-creator-text'>
@@ -52,19 +50,25 @@ function GroupCreation({ toggleVisibility, defaultGame}) {
         <div className="group-game">
           {selectedGame}
         </div>
+        
+        {/* Rank Dropdown */}
         <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic" className="group-dropdown-custom">
             {userRank}
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => handleRank('Immortal 3')}>Immortal 3</Dropdown.Item>
+            {rankOptions[selectedGame]?.map((rank, index) => (
+              <Dropdown.Item key={index} onClick={() => handleRank(rank)}>
+                {rank}
+              </Dropdown.Item>
+            ))}
           </Dropdown.Menu>
         </Dropdown>
+
         <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic" className="group-dropdown-custom">
             {groupGamemode}
           </Dropdown.Toggle>
-
           <Dropdown.Menu>
             <Dropdown.Item onClick={() => handleGamemode("Unrated")}>Unrated</Dropdown.Item>
             <Dropdown.Item onClick={() => handleGamemode("Deathmatch")}>Deathmatch</Dropdown.Item>
@@ -73,11 +77,11 @@ function GroupCreation({ toggleVisibility, defaultGame}) {
             <Dropdown.Item onClick={() => handleGamemode("Competitive")}>Competitive</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+
         <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic" className="group-dropdown-custom">
             # OF PLAYERS: {numPlayers}
           </Dropdown.Toggle>
-
           <Dropdown.Menu>
             <Dropdown.Item onClick={() => handlePlayers(2)}>2</Dropdown.Item>
             <Dropdown.Item onClick={() => handlePlayers(3)}>3</Dropdown.Item>
@@ -85,13 +89,14 @@ function GroupCreation({ toggleVisibility, defaultGame}) {
             <Dropdown.Item onClick={() => handlePlayers(5)}>5</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
-        <input className="group-title" type="text" value={groupTitle} onChange={groupTitleChange} placeholder='<ENTER GROUP TITLE>'/>
+
+        <input className="group-title" type="text" value={groupTitle} onChange={groupTitleChange} placeholder='<ENTER GROUP TITLE>' />
         <button className='group-done-button' onClick={toggleVisibility}>
           DONE
         </button>
       </div>
     </div>
   );
-};
+}
 
 export default GroupCreation;

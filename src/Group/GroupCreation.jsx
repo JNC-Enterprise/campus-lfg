7eaae9ed6c0f826f71bfc3b9fee578278967519e
Christian Walker <walkercd21@gmail.com>
2024-12-01 10:17:26 -0500

    Made it that each group creation screen per game has the game's specific ranks, gamemodes, and number of players. This is all currently stored in constants in GroupCreation.jsx if we want to move it to it's own seperate file at a later point

--- src/Group/GroupCreation.jsx
+++ src/Group/GroupCreation.jsx
@@ -11,11 +11,54 @@ function GroupCreation({ toggleVisibility, defaultGame }) {
 
   // Define rank options for each game
   const rankOptions = {
-    "Valorant": ["Iron 1", "Iron 2", "Iron 3", "Bronze 1", "Bronze 2", "Bronze 3", "Silver", "Gold", "Platinum", "Diamond", "Immortal", "Radiant"],
-    "Overwatch": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster", "Top 500"],
+    "Valorant": ["Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Immortal", "Radiant"],
+    "Overwatch": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster"],
     "Rainbow Six Siege": ["Copper", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Champion"],
+    "Fortnite": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Elite", "Champion", "Unreal"],
+    "Apex Legends": ["Rookie", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Apex Predator"],
+    "Genshin Impact": ["N/A"],
+    "Minecraft":["N/A"],
+    "Team Fortress 2": ["Mercenary", "Contract Killer" ,"Executioner", "Expert Assasin", "Death Merchant"],
+    "Counter Strike": ["Gray", "Light Blue", "Blue", "Purple", "Pink", "Red", "Gold"],
+    "Spellbreak": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Legend"],
+    "PUBG": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Crown", "Ace", "Conqueror"],
+    "Call of Duty": ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Crimson", "Iridescent", "Top 250"],
     // Add more games with specific ranks as needed
   };
+
+  // Define Gamemode options for each game
+  const gamemodeOptions = {
+    "Valorant": ["Unrated", "Competitive", "Swiftplay", "Spike Rush", "Premier", "Deathmatch", "Team Deathmatch"],
+    "Overwatch": ["Competitive", "Deathmatch", "Capture the Flag", "PVE", "Limited Time Event"],
+    "Rainbow Six Siege": ["Competitive", "Quick Match", "Standard", "Hostage", "Bomb"],
+    "Fortnite": ["Competitive (Build)", "Competitive (No Build)", "Unrated (Build)", "Unrated (No Build)", "Creative", "Team Rumble", "Lego Fortnite", "Save the World"],
+    "Apex Legends": ["Battle Royale", "Ranked", "Control", "Gun Run", "Team Deathmatch"],
+    "Genshin Impact": ["Multiplayer"],
+    "Minecraft": ["Vanilla Minecraft", "Modded Minecraft", "Server Minigames"],
+    "Team Fortress 2": ["Casual", "Competitive", "Mann vs Machine", "Custom Server"],
+    "Counter Strike": ["Premier", "Deathmatch", "Casual", "Third Party Competitive", "Custom Server Game"],
+    "Spellbreak": ["Battle Royale (Casual)", "Battle Royale (Competitive)", "Dominion"],
+    "PUBG": ["Classic (Competitive)", "Classic (Casual)", "Arcade", "Team Deathmatch"],
+    "Call of Duty": ["Quick Play", "Competitive", "Hardcore"]
+  };
+
+  // Define # of players for each game
+  const playersOptions = {
+    "Valorant": [1, 2, 3, 4],
+    "Overwatch": [1, 2, 3, 4],
+    "Rainbow Six Siege": [1, 2, 3, 4],
+    "Fortnite": [1, 2, 3],
+    "Apex Legends": [1, 2],
+    "Genshin Impact": [1, 2, 3],
+    "Minecraft": [1, 2, 3, 4, 5, 6],
+    "Team Fortress 2": [1, 2, 3, 4, 5],
+    "Counter Strike": [1, 2, 3, 4],
+    "Spellbreak": [1, 2, 3],
+    "PUBG": [1, 2, 3],
+    "Call of Duty": [1, 2, 3, 4, 5]
+  };
+  
+
 
   const handleSelect = (game) => {
     setSelectedGame(game);
@@ -70,11 +113,11 @@ function GroupCreation({ toggleVisibility, defaultGame }) {
             {groupGamemode}
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => handleGamemode("Unrated")}>Unrated</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGamemode("Deathmatch")}>Deathmatch</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGamemode("Swiftplay")}>Swiftplay</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGamemode("Spike Rush")}>Spike Rush</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleGamemode("Competitive")}>Competitive</Dropdown.Item>
+            {gamemodeOptions[selectedGame]?.map((gamemode, index) => (
+              <Dropdown.Item key={index} onClick={() => handleGamemode(gamemode)}>
+                {gamemode}
+              </Dropdown.Item>
+            ))}
           </Dropdown.Menu>
         </Dropdown>
 
@@ -83,10 +126,11 @@ function GroupCreation({ toggleVisibility, defaultGame }) {
             # OF PLAYERS: {numPlayers}
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item onClick={() => handlePlayers(2)}>2</Dropdown.Item>
-            <Dropdown.Item onClick={() => handlePlayers(3)}>3</Dropdown.Item>
-            <Dropdown.Item onClick={() => handlePlayers(4)}>4</Dropdown.Item>
-            <Dropdown.Item onClick={() => handlePlayers(5)}>5</Dropdown.Item>
+            {playersOptions[selectedGame]?.map((players, index) => (
+              <Dropdown.Item key={index} onClick={() => handlePlayers(players)}>
+                {players}
+              </Dropdown.Item>
+            ))}
           </Dropdown.Menu>
         </Dropdown>
 

cfdfaa1efb32e355ca0236ae24637c43fd642ea4
Jatin Suri <surijatin@yahoo.com>
2024-12-18 20:28:38 -0500

    fixed qoutations

--- backend/SQLScripts/second.sql
+++ backend/SQLScripts/second.sql
@@ -1,3 +1,14 @@
 INSERT INTO Game (game_id, title, genre, platform) 
-
-VALUES (1, 'Valorant', 'FPS', 'PC'), (2, ‘Overwatch 2’, ‘Shooter’, 'PC'), (3, ‘Rainbow Six Siege’, 'Shooter’, 'PC'), (4, 'Fortnite', 'Battle Royale', 'Multi-platform'), (5, ‘Genshin Impact’ , ‘RPG’, 'Multi-platform'), (6, ‘Minecraft’, ’Sandbox’, 'Multi-platform'), (7, ‘Team fortress 2’, ‘Shooter’, 'Multi-platform'), (8, 'Counter-Strike 2', 'FPS', 'PC'), (9, ‘Spellbreak’, ‘Battle Royale’, 'PC'), (10, ‘PUBG’, ‘Battle Royale’, 'Multi-platform’), (11, ‘Call of Duty’, ‘Shooter’, 'Multi-platform’), (13, ‘Apex Legends’ , ‘Battle Royale’, 'Multi-platform');
+VALUES 
+  (1, 'Valorant', 'FPS', 'PC'),
+  (2, 'Overwatch 2', 'Shooter', 'PC'),
+  (3, 'Rainbow Six Siege', 'Shooter', 'PC'),
+  (4, 'Fortnite', 'Battle Royale', 'Multi-platform'),
+  (5, 'Genshin Impact', 'RPG', 'Multi-platform'),
+  (6, 'Minecraft', 'Sandbox', 'Multi-platform'),
+  (7, 'Team Fortress 2', 'Shooter', 'Multi-platform'),
+  (8, 'Counter-Strike 2', 'FPS', 'PC'),
+  (9, 'Spellbreak', 'Battle Royale', 'PC'),
+  (10, 'PUBG', 'Battle Royale', 'Multi-platform'),
+  (11, 'Call of Duty', 'Shooter', 'Multi-platform'),
+  (13, 'Apex Legends', 'Battle Royale', 'Multi-platform');
